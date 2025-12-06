4e72bdd97f7ecd975ca3fe27d59f759987d8c714
Fahim Ahmed Ayon <fa369383@gmail.com>
2025-12-07 01:08:49 +0600

    users get api done

--- index.js
+++ index.js
@@ -34,9 +34,28 @@ async function run() {
     const db = client.db("public_Infrastructure_user");
     const usersCollection = db.collection("users");
     // users related api
+    app.get("/users", async (req, res) => {
+      const searchText = req.query.searchText;
+      const query = {};
+      if (searchText) {
+        // query.displayName = { $regex: searchText, $options: "i" };
+
+        query.$or = [
+          { displayName: { $regex: searchText, $options: "i" } },
+          { email: { $regex: searchText, $options: "i" } },
+        ];
+      }
+
+      const cursor = usersCollection
+        .find(query)
+        .sort({ createdAt: 1 })
+        .limit(5);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       try {
-        
         const user = req.body;
         // user.role = "user";
         user.createdAt = new Date();
