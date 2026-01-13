e4ced4811fdba0190f0c2a1b76a957c5de8fb2e2
fah20im04 <fa369383@gmail.com>
2026-01-14 01:29:16 +0600

    fixed done

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ const FRONTEND_ORIGIN = process.env.SITE_DOMAIN;
 
 app.use(
   cors({
-    origin: "*",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", FRONTEND_ORIGIN],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
